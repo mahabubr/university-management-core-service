4e6befbfe4a9f74d57949394156f60da25b9c658
mahabubr <mahabub.r753@gmail.com>
2023-09-19 19:29:47 +0600

    all task done

--- src/app/modules/student/student.services.ts
+++ src/app/modules/student/student.services.ts
@@ -272,6 +272,24 @@ const myAcademicInfo = async (authUserId: string) => {
   };
 };
 
+const createStudentFromEvent = async (e: any) => {
+  const studentData: Partial<Student> = {
+    studentId: e.id,
+    firstName: e.name.firstName,
+    lastName: e.name.lastName,
+    middleName: e.name.middleName,
+    email: e.email,
+    contactNo: e.contactNo,
+    gender: e.gender,
+    bloodGroup: e.bloodGroup,
+    academicSemesterId: e.academicSemester.syncId,
+    academicDepartmentId: e.academicDepartment.syncId,
+    academicFacultyId: e.academicFaculty.syncId,
+  };
+
+  await insertIntoDB(studentData as Student);
+};
+
 export const StudentService = {
   insertIntoDB,
   getAllFromDB,
@@ -281,4 +299,5 @@ export const StudentService = {
   myCourses,
   getMyCourseSchedule,
   myAcademicInfo,
+  createStudentFromEvent,
 };
